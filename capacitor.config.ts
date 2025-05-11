b31594041aab5ee0cd819ab156bdfbf1db8a9fb2
Konformist <konstantin.yakovlev.8888@gmail.com>
2025-05-11 15:16:07 +0300

    fixed bug status bar

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,6 +4,10 @@ const config: CapacitorConfig = {
   appId: 'com.konformist.comicsreader',
   appName: 'comics-reader',
   webDir: 'dist',
+  zoomEnabled: true,
+  android: {
+    adjustMarginsForEdgeToEdge: 'auto',
+  },
   plugins: {
     CapacitorHttp: {
       enabled: true,

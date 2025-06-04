33253d06001afe56ea5b5d0c8c64301eb72742d7
Konformist <konstantin.yakovlev.8888@gmail.com>
2025-06-04 12:01:48 +0300

    update dependencies android

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -33,7 +33,7 @@ android {
     buildTypes {
         getByName("release") {
             isMinifyEnabled = true
-//            isShrinkResources = true
+            isShrinkResources = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android.txt"),
                 "proguard-rules.pro"

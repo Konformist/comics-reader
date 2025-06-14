e63005965f5c02d121186c437baa234ff7e7d539
Konformist <konstantin.yakovlev.8888@gmail.com>
2025-06-15 00:28:11 +0300

    update android version

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.konformist.comicsreader"
         minSdk = 34
         targetSdk = 35
-        versionCode = 17
-        versionName = "1.4.11"
+        versionCode = 18
+        versionName = "1.4.12"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         androidResources {
             localeFilters.addAll(listOf("en", "ru"))

b75cc8d47afd9fa18002aff3e5a1464e098f089c
Konformist <konstantin.yakovlev.8888@gmail.com>
2025-06-19 20:39:16 +0300

    fixed override

--- src/pages/comics/[id]/override-edit.vue
+++ src/pages/comics/[id]/override-edit.vue
@@ -1,127 +1,173 @@
 <template>
   <v-main>
-    <v-container class="pb-16 mb-4">
-      <v-alert
-        class="mb-4"
-        color="info"
-        variant="tonal"
-      >
-        Переопределение настроек парсера.<br>
-        Работают с DOM деревом. См. CSS.
-      </v-alert>
-      <CustomSelect
-        v-if="parsersStore.parsers.length"
-        v-model="comic.parserId"
-        class="mb-4"
-        :items="parsersStore.parsers"
-        label="Парсер"
-        @update:model-value="loadParser()"
-      />
-      <v-textarea
-        v-model.trim="comic.fromUrl"
-        auto-grow
-        :autocapitalize="false"
-        :autocomplete="false"
-        class="mb-4"
-        inputmode="url"
-        label="Ссылка на комикс"
-        rows="2"
-        @change="updateParser()"
-      />
-      <v-textarea
-        v-model.trim="keyTitle"
-        auto-grow
-        :autocapitalize="false"
-        :autocomplete="false"
-        class="mb-4"
-        :disabled="!comic.parserId"
-        inputmode="url"
-        label="CSS указатель на название"
-        rows="2"
-      />
-      <v-textarea
-        v-model.trim="keyCover"
-        auto-grow
-        :autocapitalize="false"
-        :autocomplete="false"
-        class="mb-4"
-        :disabled="!comic.parserId"
-        inputmode="url"
-        label="CSS указатель на картинку"
-        rows="2"
-      />
-      <v-textarea
-        v-model.trim="keyLanguage"
-        auto-grow
-        :autocapitalize="false"
-        :autocomplete="false"
-        class="mb-4"
-        :disabled="!comic.parserId"
-        inputmode="url"
-        label="CSS указатель на язык"
-        rows="2"
-      />
-      <v-textarea
-        v-model.trim="keyAuthors"
-        auto-grow
-        :autocapitalize="false"
-        :autocomplete="false"
-        class="mb-4"
-        :disabled="!comic.parserId"
-        inputmode="url"
-        label="CSS указатель на авторов"
-        rows="2"
-      />
-      <v-textarea
-        v-model.trim="keyAuthorsText"
-        auto-grow
-        :autocapitalize="false"
-        :autocomplete="false"
-        class="mb-4"
-        :disabled="!comic.parserId"
-        inputmode="url"
-        label="CSS указатель на текст авторов"
-        rows="2"
-      />
-      <v-textarea
-        v-model.trim="keyTags"
-        auto-grow
-        :autocapitalize="false"
-        :autocomplete="false"
-        class="mb-4"
-        :disabled="!comic.parserId"
-        inputmode="url"
-        label="CSS указатель на теги"
-        rows="2"
-      />
-      <v-textarea
-        v-model.trim="keyTagsText"
-        auto-grow
-        :autocapitalize="false"
-        :autocomplete="false"
-        class="mb-4"
-        :disabled="!comic.parserId"
-        inputmode="url"
-        label="CSS указатель на текст тегов"
-        rows="2"
-      />
-      <v-textarea
-        v-model="cookie"
-        auto-grow
-        :autocapitalize="false"
-        :autocomplete="false"
-        class="mb-4"
-        inputmode="url"
-        label="Cookie"
-        rows="2"
-      />
-      <v-btn
-        class="w-100"
-        :disabled="!comic.parserId || !comic.fromUrl || loading || loadingGlobal"
-        text="Загрузить"
-        variant="tonal"
-        @click="parseComic()"
-      />
+    <v-container class="pa-0 pb-16">
+      <div class="pa-4">
+        <v-alert
+          class="mb-4"
+          color="info"
+          variant="tonal"
+        >
+          Переопределение настроек парсера.<br>
+          Работают с DOM деревом. См. CSS.
+        </v-alert>
+        <CustomSelect
+          v-if="parsersStore.parsers.length"
+          v-model="comic.parserId"
+          class="mb-4"
+          :items="parsersStore.parsers"
+          label="Парсер"
+          @update:model-value="loadParser()"
+        />
+        <v-textarea
+          v-model.trim="comic.fromUrl"
+          auto-grow
+          :autocapitalize="false"
+          :autocomplete="false"
+          inputmode="url"
+          label="Ссылка на комикс"
+          rows="2"
+          @change="updateParser()"
+        />
+      </div>
+      <v-divider />
+      <div class="pa-4">
+        <v-textarea
+          v-model.trim="overrideData.titleCSS"
+          :autocapitalize="false"
+          :autocomplete="false"
+          class="mb-4"
+          inputmode="url"
+          label="CSS указатель на название"
+          rows="2"
+        />
+        <v-textarea
+          v-model.trim="overrideData.annotationCSS"
+          :autocapitalize="false"
+          :autocomplete="false"
+          class="mb-4"
+          inputmode="url"
+          label="CSS указатель на аннотацию"
+          rows="2"
+        />
+        <v-textarea
+          v-model.trim="overrideData.coverCSS"
+          :autocapitalize="false"
+          :autocomplete="false"
+          class="mb-4"
+          inputmode="url"
+          label="CSS указатель на изображение"
+          rows="2"
+        />
+        <v-textarea
+          v-model.trim="overrideData.authorsCSS"
+          :autocapitalize="false"
+          :autocomplete="false"
+          class="mb-4"
+          inputmode="url"
+          label="CSS указатель на авторов"
+          rows="2"
+        />
+        <v-text-field
+          v-model.trim="overrideData.authorsTextCSS"
+          :autocapitalize="false"
+          :autocomplete="false"
+          class="mb-4"
+          inputmode="url"
+          label="CSS указатель на текст авторов"
+        />
+        <v-textarea
+          v-model.trim="overrideData.languageCSS"
+          :autocapitalize="false"
+          :autocomplete="false"
+          class="mb-4"
+          inputmode="url"
+          label="CSS указатель на текст язык"
+          rows="2"
+        />
+        <v-textarea
+          v-model.trim="overrideData.tagsCSS"
+          :autocapitalize="false"
+          :autocomplete="false"
+          class="mb-4"
+          inputmode="url"
+          label="CSS указатель на теги"
+          rows="2"
+        />
+        <v-text-field
+          v-model.trim="overrideData.tagsTextCSS"
+          :autocapitalize="false"
+          :autocomplete="false"
+          inputmode="url"
+          label="CSS указатель на текст тегов"
+        />
+      </div>
+      <v-divider />
+      <div class="pa-4">
+        <v-textarea
+          v-model.trim="overrideData.chaptersCSS"
+          :autocapitalize="false"
+          :autocomplete="false"
+          class="mb-4"
+          inputmode="url"
+          label="CSS указатель на главы"
+          rows="2"
+        />
+        <v-textarea
+          v-model.trim="overrideData.chaptersTitleCSS"
+          :autocapitalize="false"
+          :autocomplete="false"
+          class="mb-4"
+          inputmode="url"
+          label="CSS указатель на название главы"
+          rows="2"
+        />
+        <v-textarea
+          v-model.trim="overrideData.pagesTemplateUrl"
+          :autocapitalize="false"
+          :autocomplete="false"
+          class="mb-4"
+          inputmode="url"
+          label="Шаблон ссылок для страниц"
+          rows="2"
+        />
+        <v-textarea
+          v-model.trim="overrideData.pagesCSS"
+          :autocapitalize="false"
+          :autocomplete="false"
+          class="mb-4"
+          inputmode="url"
+          label="CSS указатель на страницы"
+          rows="2"
+        />
+        <v-textarea
+          v-model.trim="overrideData.pagesImageCSS"
+          :autocapitalize="false"
+          :autocomplete="false"
+          inputmode="url"
+          label="CSS указатель на изображение страницы"
+          rows="2"
+        />
+      </div>
+      <v-divider />
+      <div class="pa-4">
+        <v-textarea
+          v-model="cookie"
+          auto-grow
+          :autocapitalize="false"
+          :autocomplete="false"
+          class="mb-4"
+          inputmode="url"
+          label="Cookie"
+          rows="2"
+        />
+        <v-btn
+          class="w-100"
+          :disabled="!comic.parserId || !comic.fromUrl || loading || loadingGlobal"
+          text="Загрузить"
+          variant="tonal"
+          @click="parseComic()"
+        />
+      </div>
     </v-container>
     <v-fab
       :disabled="loading || loadingGlobal"
@@ -139,6 +185,7 @@ import ComicController from '@/core/entities/comic/ComicController.ts';
 import ComicModel from '@/core/entities/comic/ComicModel.ts';
 import ParserController from '@/core/entities/parser/ParserController.ts';
 import ParserModel from '@/core/entities/parser/ParserModel.ts';
+import type { IParseData } from '@/plugins/WebApiPlugin.ts';
 import { useAuthorsStore } from '@/stores/authors.ts';
 import { useComicsStore } from '@/stores/comics.ts';
 import { useLanguagesStore } from '@/stores/languages.ts';
@@ -208,68 +255,86 @@ const onSave = async () => {
   }
 };
 
-const keyTitle = computed({
-  get() {
+const overrideData = computed<IParseData>(() => ({
+  get titleCSS() {
     return comicOverride.value.titleCSS || parser.value.titleCSS;
   },
-  set(value) {
+  set titleCSS(value) {
     comicOverride.value.titleCSS = value;
   },
-});
-
-const keyCover = computed({
-  get() {
+  get annotationCSS() {
+    return comicOverride.value.annotationCSS || parser.value.annotationCSS;
+  },
+  set annotationCSS(value) {
+    comicOverride.value.annotationCSS = value;
+  },
+  get coverCSS() {
     return comicOverride.value.coverCSS || parser.value.coverCSS;
   },
-  set(value) {
+  set coverCSS(value) {
     comicOverride.value.coverCSS = value;
   },
-});
-
-const keyLanguage = computed({
-  get() {
-    return comicOverride.value.languageCSS || parser.value.languageCSS;
-  },
-  set(value) {
-    comicOverride.value.languageCSS = value;
-  },
-});
-
-const keyAuthors = computed({
-  get() {
+  get authorsCSS() {
     return comicOverride.value.authorsCSS || parser.value.authorsCSS;
   },
-  set(value) {
+  set authorsCSS(value) {
     comicOverride.value.authorsCSS = value;
   },
-});
-
-const keyAuthorsText = computed({
-  get() {
+  get authorsTextCSS() {
     return comicOverride.value.authorsTextCSS || parser.value.authorsTextCSS;
   },
-  set(value) {
+  set authorsTextCSS(value) {
     comicOverride.value.authorsTextCSS = value;
   },
-});
-
-const keyTags = computed({
-  get() {
+  get languageCSS() {
+    return comicOverride.value.languageCSS || parser.value.languageCSS;
+  },
+  set languageCSS(value) {
+    comicOverride.value.languageCSS = value;
+  },
+  get tagsCSS() {
     return comicOverride.value.tagsCSS || parser.value.tagsCSS;
   },
-  set(value) {
+  set tagsCSS(value) {
     comicOverride.value.tagsCSS = value;
   },
-});
-
-const keyTagsText = computed({
-  get() {
+  get tagsTextCSS() {
     return comicOverride.value.tagsTextCSS || parser.value.tagsTextCSS;
   },
-  set(value) {
+  set tagsTextCSS(value) {
     comicOverride.value.tagsTextCSS = value;
   },
-});
+  get chaptersCSS() {
+    return comicOverride.value.chaptersCSS || parser.value.chaptersCSS;
+  },
+  set chaptersCSS(value) {
+    comicOverride.value.chaptersCSS = value;
+  },
+  get chaptersTitleCSS() {
+    return comicOverride.value.chaptersTitleCSS || parser.value.chaptersTitleCSS;
+  },
+  set chaptersTitleCSS(value) {
+    comicOverride.value.chaptersTitleCSS = value;
+  },
+  get pagesTemplateUrl() {
+    return comicOverride.value.pagesTemplateUrl || parser.value.pagesTemplateUrl;
+  },
+  set pagesTemplateUrl(value) {
+    comicOverride.value.pagesTemplateUrl = value;
+  },
+  get pagesCSS() {
+    return comicOverride.value.pagesCSS || parser.value.pagesCSS;
+  },
+  set pagesCSS(value) {
+    comicOverride.value.pagesCSS = value;
+  },
+  get pagesImageCSS() {
+    return comicOverride.value.pagesImageCSS || parser.value.pagesImageCSS;
+  },
+  set pagesImageCSS(value) {
+    comicOverride.value.pagesImageCSS = value;
+  },
+}));
 
 const init = async () => {
   loadingStart();
